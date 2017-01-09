bc13a1659f82f494c5d08bb4a1f2e2e402979534
konradschlatte <konrad.schlatte@btinternet.com>
2017-01-09 16:40:49 +0000

    initialize and attr_reader

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
-
+require "./lib/oystercard"
 describe Oystercard do
 
+  #subject(:oystercard) {described_class.new(100)}
+
+  it ' should show oystercard balance' do
+
+  card = Oystercard.new(100)
+  expect(card.balance).to eq(100)
+  end
 end
